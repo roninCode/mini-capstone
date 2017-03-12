738ab1eae105747b3aeb5fd68d817618bd277bb7
Jamie MacKillop <jamieronin@gmail.com>
2017-03-12 10:46:36 -0400

    Add new book instances and show new instance

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -10,5 +10,21 @@ class BooksController < ApplicationController
     render 'show.html.erb'
   end
 
-  
+  def new
+    render 'submit_here.html.erb'
+  end
+ 
+  def create
+    @book = Book.new(
+      title:  params['title'],
+      author: params['author'],
+      price: params['price'],
+      image: params['image'],
+      description: params['description'],
+      copyright_year:  params['copyright_year'],
+    )
+    @book.save
+    render 'post_new_book.html.erb'
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get '/' => 'books#index'
   get '/books' => 'books#index'
+  get '/books/new' => 'books#new'
+  post '/books' => 'books#create'
   get '/book/:id' => 'books#show'
-  get '/books' => 'books#s'
 end
